3646f48d79ffa6018279221c797e102b405e0d06
dreiflos <andreifloresrodrigues@gmail.com>
2026-02-21 16:19:34 -0400

    Finalização

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
-
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        Scanner leitor = new Scanner(System.in);
 
-        Funcionario func1 = new Funcionario("Andrei", "Flores", "3333-33", "rua jasmin", new Date(), null, new Date());
+        System.out.print("Digite o primeiro nome do funcionário: ");
+        String nomeDigitado = leitor.nextLine();
+        System.out.print("Digite o sobrenome do funcionário: ");
+        String sobrenomeDigitado = leitor.nextLine();
+
+        System.out.println("\nGerando holerite de " + nomeDigitado + "\n");
+
+        Funcionario func1 = new Funcionario(nomeDigitado, sobrenomeDigitado, "3333-33", "rua jasmin", new Date(), null, new Date());
 
         Cargo cargoFunc1 = new Cargo(3000.00, "Dev", "desenvolvimento", 230.00);
-
-        double horasTrabalhadas = 230.0;
-
-        Hora hora1 = new Hora(35.0, 35.0);
-
-        Pagamento pagamentoMes = new Pagamento(func1, cargoFunc1, horasTrabalhadas);
-
+        Hora hora1 = new Hora(15.0, 5.0);
+        Pagamento pagamentoMes = new Pagamento(func1, cargoFunc1, hora1);
         pagamentoMes.gerarHolerite();
 
+        leitor.close();
     }
-
 }
--- src/Pagamento.java
+++ src/Pagamento.java
@@ -1,13 +1,13 @@
 public class Pagamento {
     private Funcionario funcionario;
     private Cargo cargo;
-    private double horasTrabalhadas;
+    private Hora horasMes;
     private Imposto calcular = new Imposto();
 
-    public Pagamento (Funcionario funcionario, Cargo cargo, double horasTrabalhadas) {
+    public Pagamento (Funcionario funcionario, Cargo cargo, Hora horasMes) {
         this.cargo = cargo;
         this.funcionario = funcionario;
-        this.horasTrabalhadas = horasTrabalhadas;
+        this.horasMes = horasMes;
     }
 
     public void gerarHolerite() {
@@ -18,28 +18,25 @@ public class Pagamento {
 
         double valorHora = salario / horasFixas;
 
-        double saldoHoras = horasTrabalhadas - horasFixas;
+        double valorExtras = horasMes.getHorasExtras() * valorHora;
+        double valorFaltas = horasMes.getHorasFaltantes() * valorHora;
 
-        double salarioBruto = salario + (saldoHoras * valorHora);
+        double salarioBruto = salario + valorExtras - valorFaltas;
 
         double desconto = calcular.calcular(salarioBruto);
 
         double salarioLiquido = salarioBruto - desconto;
 
-
-        System.out.println("====================================");
         System.out.println("          HOLERITE MENSAL           ");
-        System.out.println("====================================");
         System.out.println("Funcionário: " + nomeCompleto);
         System.out.println("Cargo: " + nomeCargo);
-        System.out.println("Salário Base: R$ " + salario + " (" + horasFixas + "h)");
-        System.out.println("Horas Realizadas: " + horasTrabalhadas + "h");
+        System.out.println("Salário Base: R$ " + String.format("%.2f", salario) + " (" + horasFixas + "h)");
         System.out.println("------------------------------------");
-        System.out.println("Salário Bruto Calculado: R$ " + salarioBruto);
-        System.out.println("Desconto Imposto: R$ " + desconto);
+        System.out.println("Horas Extras (" + horasMes.getHorasExtras() + "h): + R$ " + String.format("%.2f", valorExtras));
+        System.out.println("Faltas (" + horasMes.getHorasFaltantes() + "h): - R$ " + String.format("%.2f", valorFaltas));
+        System.out.println("Salário Bruto Calculado: R$ " + String.format("%.2f", salarioBruto));
+        System.out.println("Desconto Imposto: R$ " + String.format("%.2f", desconto));
         System.out.println("------------------------------------");
-        System.out.println("SALÁRIO LÍQUIDO A RECEBER: R$ " + salarioLiquido);
-        System.out.println("====================================");
+        System.out.println("SALÁRIO LÍQUIDO A RECEBER: R$ " + String.format("%.2f", salarioLiquido));
     }
-
 }
